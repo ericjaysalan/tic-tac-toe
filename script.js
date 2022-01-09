6578eeecfeee54479596b5f1dc7e7453d8e5800f
Eric Jay Salan <ericjaysalan@yahoo.com>
2022-01-09 23:02:34 +0800

    Game will now check for a winner  everytime a player places a marker

--- script.js
+++ script.js
@@ -2,24 +2,65 @@ const gameBoard = (() => {
   const board = document.getElementById('game-board');
   const cells = Array.from(board.children);
 
-  const _cellHasNoMarker = (cell) => cell.innerText === '';
-
-  const _setMarker = (cell, player) => {
-    const marker = player.getMarker();
+  const _setMarker = cell => {
+    const marker = game.getCurrentPlayer().getMarker();
     cell.innerText = marker;
     cell.classList.add('active');
-    game.nextPlayer();
   };
 
-  const _checkCell = (e) => {
+  const _checkBoardHorizontally = (marks, mark) => {
+    for (let i = 0; i <= marks.length - 3; i += 3) {
+      if (marks[i] === mark && marks[i + 1] === mark && marks[i + 2] === mark) return true;
+    }
+
+    return false;
+  };
+
+  const _checkBoardVertically = (marks, mark) => {
+    for (let i = 0; i < 3; i++) {
+      if (marks[i] === mark && marks[i + 3] === mark && marks[i + 6] === mark) return true;
+    }
+
+    return false;
+  };
+
+  const _checkBoardDiagonally = (marks, mark) => {
+    if (
+      (marks[0] === mark && marks[4] === mark && marks[8] === mark) ||
+      (marks[2] === mark && marks[4] && marks[6])
+    )
+      return true;
+    else return false;
+  };
+
+  const _checkWinner = () => {
+    const currentPlayer = game.getCurrentPlayer();
+    const marks = cells.map(cell => cell.innerText);
+    const mark = currentPlayer.getMarker();
+
+    if (
+      _checkBoardVertically(marks, mark) ||
+      _checkBoardHorizontally(marks, mark) ||
+      _checkBoardDiagonally(marks, mark)
+    )
+      return true;
+  };
+
+  const _cellHasNoMarker = cell => cell.innerText === '';
+
+  const _checkCell = e => {
     const cell = e.target;
 
-    if (_cellHasNoMarker(cell)) _setMarker(cell, game.getCurrentPlayer());
-    else console.log('nope!');
+    if (_cellHasNoMarker(cell)) {
+      _setMarker(cell);
+
+      if (_checkWinner()) game.end();
+      else game.nextPlayer();
+    }
   };
 
   const setClickListenerToCells = () => {
-    cells.forEach((cell) => {
+    cells.forEach(cell => {
       cell.addEventListener('click', _checkCell);
     });
   };
@@ -33,15 +74,15 @@ const game = (() => {
   const players = [];
   let currentPlayer;
 
-  const setPlayers = (playersParam) => {
-    playersParam.forEach((player) => players.push(player));
+  const setPlayers = playersParam => {
+    playersParam.forEach(player => players.push(player));
     currentPlayer = players[0];
   };
 
   const getPlayers = () => players;
 
   const nextPlayer = () => {
-    const currentPlayerIndex = players.findIndex((player) => player === currentPlayer);
+    const currentPlayerIndex = players.findIndex(player => player === currentPlayer);
     // Assuming there are 2 players in a game
     if (currentPlayerIndex === 0) currentPlayer = players[1];
     else currentPlayer = players[0];
@@ -54,19 +95,23 @@ const game = (() => {
     console.log('game set');
   };
 
+  const end = () => {
+    console.log(currentPlayer.getName());
+  };
   return {
     setPlayers,
     getPlayers,
     nextPlayer,
     getCurrentPlayer,
     setGame,
+    end,
   };
 })();
 
 const Player = (name, marker) => {
-  const setName = (param) => (name = param);
+  const setName = param => (name = param);
   const getName = () => name;
-  const setMarker = (param) => (marker = param);
+  const setMarker = param => (marker = param);
   const getMarker = () => marker;
 
   return {
@@ -77,7 +122,7 @@ const Player = (name, marker) => {
   };
 };
 
-const player1 = Player('Eric Jay', 'X');
-const player2 = Player('Ej', 'O');
+const player1 = Player('Player 1', 'X');
+const player2 = Player('Player 2', 'O');
 game.setPlayers([player1, player2]);
 game.setGame();
